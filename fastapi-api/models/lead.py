ae91490e82f50cceba7a4b2705ce95b75c9fc419
Gabriel Valenga <gabrielvalengalemos@yahoo.com>
2026-08-28 08:59:49 -0300

    reafctor: Moving leads model code to correct file, deleting requirements.txt created at root level, and creating public_id in leads mdoel

---
+++ fastapi-api/models/lead.py
@@ -0,0 +1,29 @@
+from datetime import datetime, timezone
+import uuid #TODO: change to import uuid6 and in the columns below use uuid6.uuid7()
+from sqlalchemy import DateTime, String, Text, func
+from sqlalchemy.orm import Mapped, mapped_column
+from core.database import base
+
+class Lead(base):
+    __tablename__ = "leads"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    public_id: Mapped[uuid.UUID] = mapped_column(default=uuid.uuid4, unique=True, index=True)
+    first_name: Mapped[str] = mapped_column(String(20), nullable=False)
+    last_name: Mapped[str] = mapped_column(String(40), nullable=False)
+    email: Mapped[str] = mapped_column(String(100), unique=True, nullable=False, index=True)
+    company: Mapped[str | None] = mapped_column(String(50))
+    job_title: Mapped[str | None] = mapped_column(String(50))
+    notes: Mapped[str | None] = mapped_column(Text)
+    source: Mapped[str] = mapped_column(String(20), nullable=False, index=True)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        nullable=False
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        onupdate=lambda: datetime.now(timezone.utc),
+        nullable=False
+    )
